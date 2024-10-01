61e821d485605b2c6c1e8ccc04c3549e2199f2ae
Thejas0604 <meddepolamact.21@uom.lk>
2024-10-01 19:31:27 +0530

    fix: readme.md, add variable to main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from browser_factory import BrowserFactory
 from scraper import Scraper
 from utils import save_to_csv, save_to_html
 
+what_to_scrape = "PS5 Games"
+
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
@@ -10,7 +12,7 @@ def main():
     browser = BrowserFactory.create_browser(headless=True)
     scraper = Scraper(browser)
     try:
-        scraper.search_amazon("PS5 Games")
+        scraper.search_amazon(what_to_scrape)
         products = scraper.scrape_amazon_products()
         save_to_csv(products)
         save_to_html(products)
--- readme.md
+++ readme.md
@@ -46,11 +46,12 @@ The Amazon Product Scraper is a Python-based tool that uses Selenium to scrape p
     pip install -r requirements.txt
     ```
 ## Usage
-1. **Run the scraper:**
+1. **Edit ``what_to_scrape`` variable in the ``main.py`` file.**
+2. **Run the scraper:**
     ```sh
     python main.py
     ```
-2. **View the results**
+3. **View the results**
 The scraped data will be saved in ``amazon_products.csv``.
 An HTML file named ``amazon_products.html`` will be generated to display the data in a table format.
 
